c799628742b3cd34ce9fa4722fb0ff26b1f51595
Ryan Durrant <rddurrant@gmail.com>
2026-03-24 20:45:53 -0600

    horizontal scrolling works now

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, "index.html"),
+    },
+  },
+});
